a05ef5ebb61f996ebbd1b6cdfad2d318f728c7a1
Tony Dai <S65861@gmail.com>
2017-12-03 18:08:06 +1100

    V0.0

---
+++ Beam_Project/classes/Internal_force.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beam_Project.classes
+{
+    class Internal_force
+    {
+        private double moment { get; set; }
+        private double x_force { get; set; }
+        private double y_force { get; set; }
+
+        internal Internal_force(char joint_type)
+        {
+            if(joint_type == 'r') //roller joint
+            {
+                this.moment = 0;
+                this.x_force = 0;
+            } else if (joint_type == 'p') //pin joint
+            {
+                this.moment = 0;
+            }
+        }
+    }
+}
